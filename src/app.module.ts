504b8e9011f3f047eb17f3fcaec4ee77ab553564
Cyrine Ben Hlila <syrinebh05@gmail.com>
2026-02-21 22:11:35 +0100

    Initial NestJS project and config database (mongo)
    Add baseline NestJS application and tooling: source files (controllers, service, main), tests, project configs (tsconfig, tsconfig.build, nest-cli), lint/format configs (eslint.config.mjs, .prettierrc), README, .gitignore, notes, and package.json to set up dependencies and development scripts.

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+        dbName: config.get<string>('MONGODB_NAME'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/app.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+@Injectable()
+export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(@InjectConnection() private readonly connection: Connection) { }
+
+  onModuleInit() {
+    this.connection.once('open', () => {
+      this.logger.log('MongoDB connecté avec succès !');
+    });
+
+    this.connection.on('error', (err) => {
+      this.logger.error(`Erreur de connexion à MongoDB : ${err.message}`);
+    });
+  }
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+}
