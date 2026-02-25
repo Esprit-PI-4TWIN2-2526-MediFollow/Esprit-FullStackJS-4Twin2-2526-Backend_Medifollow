536291f11bd1923e3093051b6d7e0776c05667b3
YAHYA KAROUI <yahyakaroui@esprit.tn>
2026-02-25 01:41:57 +0100

    change path to /api

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -4,7 +4,7 @@ import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { Roles } from './decorator/role.decorator';
 
-@Controller('api/roles')
+@Controller('api')
 // @UseGuards(JwtAuthGuard, RolesGuard)
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
